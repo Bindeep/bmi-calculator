73ab10817801ae888624fbf7a9fea7a4866172af
bindeep.acharya <bindeep.acharya@aayulogic.com>
2021-08-05 12:24:10 +0545

    test added

--- apps/bmi/tests.py
+++ apps/bmi/tests.py
@@ -1,3 +1,48 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.urls import reverse
+from rest_framework.test import APITestCase
+
+from apps.bmi.utils import UNDERWEIGHT, HEALTHY, OVERWEIGHT, SEVERELY_OVERWEIGHT, OBESE, SEVERELY_OBESE
+
+
+class BMICalculateTest(APITestCase):
+
+    def setUp(self) -> None:
+        super().setUp()
+        self.url = reverse('api_v1:bmi:bmi-calculate')
+
+    def test_bmi_calculate(self):
+        self.assertEqual(
+            self.bmi_calculate(10, 100)['weight_group'],
+            UNDERWEIGHT
+        )
+        self.assertEqual(
+            self.bmi_calculate(20, 100)['weight_group'],
+            HEALTHY
+        )
+        self.assertEqual(
+            self.bmi_calculate(28, 100)['weight_group'],
+            OVERWEIGHT
+        )
+        self.assertEqual(
+            self.bmi_calculate(33, 100)['weight_group'],
+            SEVERELY_OVERWEIGHT
+        )
+        self.assertEqual(
+            self.bmi_calculate(38, 100)['weight_group'],
+            OBESE
+        )
+        self.assertEqual(
+            self.bmi_calculate(40, 100)['weight_group'],
+            SEVERELY_OBESE
+        )
+
+    def bmi_calculate(self, weight: int, height: int) -> dict:
+        data = {
+            'weight': weight,
+            'height': height
+        }
+        response = self.client.post(path=self.url, data=data, format='json')
+        return response.data
--- apps/bmi/utils.py
+++ apps/bmi/utils.py
@@ -2,21 +2,29 @@ def calculate_bmi(weight: int, height: int) -> float:
     return weight / (height / 100) ** 2
 
 
+UNDERWEIGHT = "You are underweight."
+HEALTHY = "You are healthy."
+OVERWEIGHT = "You are over weight."
+SEVERELY_OVERWEIGHT = "You are severely over weight."
+OBESE = "You are obese."
+SEVERELY_OBESE = "You are severely obese."
+
+
 def get_weight_group(bmi: float) -> str:
     """
     Calculate Weight Group w.r.to bmi
     """
     if bmi <= 18.4:
-        weight_group = "You are underweight."
+        weight_group = UNDERWEIGHT
     elif bmi <= 24.9:
-        weight_group = "You are healthy."
+        weight_group = HEALTHY
     elif bmi <= 29.9:
-        weight_group = "You are over weight."
+        weight_group = OVERWEIGHT
     elif bmi <= 34.9:
-        weight_group = "You are severely over weight."
+        weight_group = SEVERELY_OVERWEIGHT
     elif bmi <= 39.9:
-        weight_group = "You are obese."
+        weight_group = OBESE
     else:
-        weight_group = "You are severely obese."
+        weight_group = SEVERELY_OBESE
 
     return weight_group
